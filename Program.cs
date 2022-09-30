99ef0a0f45ffc269ae184dbaa1149acc391d674e
DiegoCast11 <92958125+DiegoCast11@users.noreply.github.com>
2022-09-30 13:08:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Act_2_CD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Actividad 2 Centro de Desarrollo \n Diego Noé Castillo Santos");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
